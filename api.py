7d447a8a9b3a64bb5926fd2cd5de74298fd86d02
Nawxtz <pongsapakdairoop48@gmail.com>
2026-04-18 16:15:03 +0700

    fix webpage bug

--- api.py
+++ api.py
@@ -669,4 +669,4 @@ async def server_info(request: Request) -> InfoResponse:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
